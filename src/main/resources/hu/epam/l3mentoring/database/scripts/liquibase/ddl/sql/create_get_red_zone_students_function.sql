548f8b6d99ed891fea0b879cb4543d792c3a1ede
Zsolt Fehér <zsolt_feher1@epam.com>
2023-09-18 17:38:53 +0200

    Create get_red_zone_students function

---
+++ src/main/resources/hu/epam/l3mentoring/database/scripts/liquibase/ddl/sql/create_get_red_zone_students_function.sql
@@ -0,0 +1,8 @@
+CREATE OR REPLACE FUNCTION get_red_zone_students()
+RETURNS SETOF student AS $$
+SELECT s.* FROM student s WHERE s.id IN
+	(SELECT er.student_id
+	   FROM exam_result er
+	  WHERE er.mark <= 3
+   GROUP BY er.student_id HAVING COUNT(er.student_id) >= 2);
+$$ LANGUAGE sql;

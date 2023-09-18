43e2134f6346ab625c0e4ea18e81854b40fd4ce6
Zsolt Fehér <zsolt_feher1@epam.com>
2023-09-18 17:30:53 +0200

    Create students_average_mark function

---
+++ src/main/resources/hu/epam/l3mentoring/database/scripts/liquibase/ddl/sql/create_students_average_mark_function.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE FUNCTION students_average_mark (p_name varchar)
+RETURNS integer AS $average_mark$
+   DECLARE
+      average_mark integer;
+   BEGIN
+   	  SELECT AVG(er.mark) INTO average_mark
+	    FROM exam_result er, student s
+	   WHERE s.id = er.student_id
+	     AND s.name = p_name;
+      RETURN avarage_mark;
+   END;
+   $average_mark$ LANGUAGE plpgsql;
